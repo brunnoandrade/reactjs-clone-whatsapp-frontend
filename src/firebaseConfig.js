2d6f81c99a1da898a81ab8b16dc41f8ed26215b6
Bruno Andrade <brunnoandradi@gmail.com>
2021-01-26 18:34:52 -0300

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBKnmHCuSHYAzifxWlevvVdzKmStfN42_o",
+    authDomain: "whatsapp-clone-eff09.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-eff09.firebaseio.com",
+    projectId: "whatsapp-clone-eff09",
+    storageBucket: "whatsapp-clone-eff09.appspot.com",
+    messagingSenderId: "809420050490",
+    appId: "1:809420050490:web:2d12ca6bfc83ce1b922503"
+};
